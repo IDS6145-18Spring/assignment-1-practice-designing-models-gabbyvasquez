fd16c2bb143f5f15c09241a74e3caa0f135f8075
gabbyvasquez <33968307+gabbyvasquez@users.noreply.github.com>
2018-01-18 18:07:36 -0500

    Add files via upload

---
+++ code/strays/AI.py
@@ -0,0 +1,27 @@
+from AI import AI
+
+class stray animal(AI): #name
+    '''This is a string bean'''
+
+    def __init__(self):
+        '''Intializes the animal'''
+        self.nonmirco = 1  # 1 indictaes yes for mirco chip #attributes
+			if yes: 
+				print(stray)
+			else:
+				print(maybe) 
+        self.minbodyfat =< .20 #attributes
+			if minbodyfat <= : 
+				print('stray')
+			else:
+				print('not stray')
+        animal.__init__(self, c, d)
+
+
+    def aloof(self): 
+        '''behavior of an animal'''
+	def afarid(self):
+		'''behavior of a stray animal'''
+	def friendly(self): 
+		'''behavior of a stray animal'''
+
